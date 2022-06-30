79909d4dad3f22a9c7a33c5f4a5b7a63b7932d95
CodeWalker3 <gustavootaku2022@gmail.com>
2022-06-30 20:08:40 -0300

    first commit

---
+++ requests.py
@@ -0,0 +1,26 @@
+import requests
+def findall():
+    api_url = "http://192.168.0.105:8090/countries"
+    response = requests.get(api_url)
+    print (response.json())
+def create():
+    api_url = f"http://192.168.0.105:8090/countries"
+    lista = {"id": 4, "name": "China", "capital": "Hong kong", "area": 51}
+    response = requests.post(api_url,json= lista)
+    print (response.json())
+def findbyid(id):
+    api_url = f"http://192.168.0.105:8090/countries/{id}"
+    response = requests.get(api_url)
+    print (response.json())
+def update(id):
+    insert = {"id": 5, "name": "e", "capital": "BevellyHills", "area": 51}
+    api_url = f"http://192.168.0.105:8090/countries/{id}"
+    response = requests.put(api_url, json= insert)
+    print (response.json())
+def delete(id):
+    api_url = f"http://192.168.0.105:8090/countries/{id}"
+    response = requests.delete(api_url)
+    print (response.json())
+
+
+findall()
